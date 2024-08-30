a51e36bb56d9715b7bf3103f77cff5ddf39677e7
UandesGIAN <gbobadilla@miuandes.cl>
2024-08-30 02:28:18 -0400

    finished home

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,4 +1,5 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
@@ -9,9 +10,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
+      colors: {
+        orange: colors.orange,
+        lime: colors.lime,
+      }
     },
   },
   plugins: [
