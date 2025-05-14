ba4573afeb52a34592ed80d4ef3d142c1da2deb7
UandesGIAN <gbobadilla@miuandes.cl>
2025-05-14 14:52:52 -0400

    dark mode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :set_mode_light
+
+  private
+
+  def set_mode_light
+    # Establecer el valor de la cookie en una variable de instancia
+    @modo_luz = cookies[:modo_luz] || "claro"  # "claro" es el valor por defecto
+  end
+  
 end
